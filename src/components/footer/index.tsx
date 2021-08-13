7a39ed90ad7bce133b8357b59a67ffe3b181492f
H-Richard <r.hong@pm.me>
2021-08-12 22:51:12 -0400

    fix(footer): open footer links in new tab

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,8 +6,18 @@ const Footer = () => (
   <div className="footer">
     <p>
       You can get in touch with us by email at{" "}
-      <a href="mailto:maiachess@cs.toronto.edu">maiachess@cs.toronto.edu</a> or
-      on Twitter <a href="https://twitter.com/maiachess">@maiachess</a>.
+      <a
+        href="mailto:maiachess@cs.toronto.edu"
+        target="_blank"
+        rel="noreferrer"
+      >
+        maiachess@cs.toronto.edu
+      </a>{" "}
+      or on Twitter{" "}
+      <a href="https://twitter.com/maiachess" target="_blank" rel="noreferrer">
+        @maiachess
+      </a>
+      .
     </p>
   </div>
 );
