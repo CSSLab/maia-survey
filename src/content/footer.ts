da822e9b9375e445b7991562fa925e6a48d368c3
H-Richard <r.hong@pm.me>
2021-08-15 22:10:06 -0400

    content(footer): update footer email
    Co-authored-by: Reidmcy <me@reidmcy.com>

--- src/content/footer.ts
+++ src/content/footer.ts
@@ -1,5 +1,5 @@
 const content = `
-You can get in touch with us by email at [maiachess@cs.toronto.edu](mailto:maiachess@cs.toronto.edu) or on Twitter [@maiachess](https://twitter.com/maiachess). 
+You can get in touch with us by email at [maiasurvey@cs.toronto.edu](mailto:maiasurvey@cs.toronto.edu) or on Twitter [@maiachess](https://twitter.com/maiachess). 
 
 Learn more about the [paper](/#paper) and [acknowledgements](/#acknowledgements) on our [main page](/).
 `;
