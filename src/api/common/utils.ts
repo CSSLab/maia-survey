30a717d91717afbdd3dc1a4936cc3402973a158d
H-Richard <r.hong@pm.me>
2021-08-16 20:28:10 -0400

    chore: remove unused imports

--- src/api/common/utils.ts
+++ src/api/common/utils.ts
@@ -1,5 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-import { useEffect, useState } from "react";
 
 export const buildUrl = (path: string) =>
   process.env.REACT_APP_API_BASE_URL + path;
