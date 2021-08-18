c3910832bddc3198fc0c094a95b46e3db691b3bd
H-Richard <r.hong@pm.me>
2021-08-18 19:07:24 -0400

    fix(header): hide mobile header on click

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useInnerWidth } from "../../hooks";
 import { UseAuthControllerHook } from "../../hooks/auth";
@@ -14,8 +14,23 @@ const Header = ({
 }: Props) => {
   const width = useInnerWidth();
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const toggleMenu = useCallback(() => setShowMenu(!showMenu), [showMenu]);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick, true);
+    return () => document.removeEventListener("click", handleClick, true);
+  }, []);
 
   return width <= 750 ? (
     <div className="header-container">
@@ -23,8 +38,12 @@ const Header = ({
         <Link to="/">MAIA CHESS</Link>
       </h2>
       <div className="auth-container">
-        <button onClick={toggleMenu}>=</button>
-        <div className={showMenu ? "dropdown-menu-open " : "dropdown-menu"}>
+        <button ref={buttonRef} onClick={toggleMenu}>
+          =
+        </button>
+        <div
+          className={showMenu ? "dropdown-menu-open" : "dropdown-menu-closed"}
+        >
           <Link to="/turing">TURING</Link>
           <Link to="/recall">RECALL</Link>
           {userName ? (
